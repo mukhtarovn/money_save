ba973b287f2fea76bb51625baddbec2ae6f8adf0
nariman <mukhtarov.n@gmail.com>
2024-11-02 04:22:37 -0400

    new update

--- my_app/settings.py
+++ my_app/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'my_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'geekshop',
-            'USER': 'django',
-            'PASSWORD': 'sarvan030511',
-            'HOST': 'localhost',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': 'geekshop',
+    #         'USER': 'django',
+    #         'PASSWORD': 'sarvan030511',
+    #         'HOST': 'localhost',
+    #     }
 }
 
 
